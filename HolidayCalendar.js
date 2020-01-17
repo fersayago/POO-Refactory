dfa778882a5422eea7051838dfd878ad2bd5b1f9
FerSayago <sayago.fernando@gmail.com>
2020-01-17 12:11:01 -0300

    Se eliminan comentarios

--- HolidayCalendar.js
+++ HolidayCalendar.js
@@ -14,37 +14,3 @@ class HolidayCalendar{
 }
 
 module.exports = HolidayCalendar
-
-/* BORRAR
-
-const MonthDay = require("./MonthDay");
-
-
-class HolidayCalendar {
-
-    dayOfWeekHoliday = [];
-    dayOfMonthHoliday = [];
-    holidayRule = [];
-
-    isHoliday(aDate) {
-        return this.isDayOfWeekHoliday(aDate)
-            || this.isDayOfMonthHoliday(aDate);
-    }
-
-    isDayOfWeekHoliday(aDate) {
-        return this.dayOfWeekHoliday.includes(aDate.getDay())
-    }
-
-    isDayOfMonthHoliday(aDate) {
-        return this.dayOfMonthHoliday.some(monthDay => MonthDay.from(aDate).equals(monthDay));
-    }
-
-
-    makeDayOfWeekHoliday (aDayOfWeek){
-        this.dayOfWeekHoliday.push(aDayOfWeek);
-    }
-
-    makeDayOfMonthHoliday(aMonthDay){
-        this.dayOfMonthHoliday.push(aMonthDay);
-    }
-} */
